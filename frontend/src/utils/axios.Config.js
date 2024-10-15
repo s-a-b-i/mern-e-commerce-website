8b4f8460c180c7ffb044600830323f230b68f9d7
Sohaib <21ntucs1360@gmail.com>
2024-10-15 19:52:55 +0500

    minor changes

--- frontend/src/utils/axios.Config.js
+++ frontend/src/utils/axios.Config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const mainUrl = 'https://mern-e-commerce-website-ax3u.onrender.com';
+const mainUrl = 'https://mern-e-commerce-website-ax3u.onrender.com/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: mainUrl,
