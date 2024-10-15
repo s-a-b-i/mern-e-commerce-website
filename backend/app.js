1c0c0a59ac270e630f81ac55763c36d73c30e1f8
Sohaib <21ntucs1360@gmail.com>
2024-10-15 19:16:03 +0500

    deployment changes

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,13 @@ import { userRouter } from './routes/user.routes.js';
 import { productRouter } from './routes/product.routes.js';
 import { cartRouter } from './routes/cart.routes.js';
 import { searchRouter } from './routes/search.routes.js';
+import path  from 'path';
 
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
@@ -26,5 +29,11 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/cart', cartRouter);
 app.use('/api/v1/search', searchRouter);
+
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+});
 
 export { app };
